6a730ea75da15e26f9fc5a2e56267a0cab5a2fcf
LarrozaGalvez <tjlarroza@gmail.com>
2025-11-14 17:43:58 +0800

    a

--- cozytask/lib/main.dart
+++ cozytask/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cozytask/dashboard.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:cozytask/signup.dart';
@@ -20,7 +21,7 @@ class MainPage extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: const Login(),
+          child: const Dashboard(),
         ),
       ),
       debugShowCheckedModeBanner: false,
